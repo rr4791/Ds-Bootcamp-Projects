1e541a7c8f85160306a4ec3f80353811d489ee8d
Rohit Rathi <rr4791@nyu.edu>
2025-10-09 20:05:34 -0400

    Q5

---
+++ Week-1/Q5.py
@@ -0,0 +1,10 @@
+# Function to add two integers
+def sum_of_integers(a, b):
+    return a + b
+
+# Take input from user
+num1 = int(input("Enter first integer: "))
+num2 = int(input("Enter second integer: "))
+
+# Call the function and print result
+print("The sum is:", sum_of_integers(num1, num2))
