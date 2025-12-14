71a466aacda4c04d64a5bddee07b4f2debd30c09
Rohit Rathi <rr4791@nyu.edu>
2025-12-14 07:56:52 -0500

    Q7.sql

---
+++ Week-4/Q7.sql
@@ -0,0 +1,16 @@
+-- LeetCode 550: Game Play Analysis IV
+-- SQL Solution (MySQL)
+
+SELECT
+    ROUND(
+        COUNT(DISTINCT a.player_id) / (SELECT COUNT(DISTINCT player_id) FROM Activity),
+        2
+    ) AS fraction
+FROM Activity a
+JOIN (
+    SELECT player_id, MIN(event_date) AS first_date
+    FROM Activity
+    GROUP BY player_id
+) f
+ON a.player_id = f.player_id
+AND a.event_date = DATE_ADD(f.first_date, INTERVAL 1 DAY);
