8f81ebaa65a6dc0406357f0da873d7c1112996b1
Rohit Rathi <rr4791@nyu.edu>
2025-12-14 07:48:44 -0500

    Q5.sql

---
+++ Week-4/Q5.sql
@@ -0,0 +1,12 @@
+-- LeetCode 1327: List the Products Ordered in a Period
+-- SQL Solution (MySQL)
+
+SELECT
+    p.product_name,
+    SUM(o.unit) AS unit
+FROM Products p
+JOIN Orders o
+ON p.product_id = o.product_id
+WHERE o.order_date BETWEEN '2020-02-01' AND '2020-02-29'
+GROUP BY p.product_id, p.product_name
+HAVING SUM(o.unit) >= 100;
